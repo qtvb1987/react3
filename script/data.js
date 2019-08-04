83f94ba94e983b9d38b041f94b82a1bf38457f65
qtvb1987 <qtvb1987@163.com>
2019-08-04 22:06:14 +0800

    react demo

---
+++ script/data.js
@@ -0,0 +1,18 @@
+var dataList={
+    friends:{
+        name:'好友',
+        list:[
+            {username:'aaa',message:'testaaa',vip:true},
+            {username:'bbb',message:'testbbb',vip:false},
+            {username:'ccc',message:'testccc',vip:false},
+        ]
+    },
+    families:{
+        name:'家人',
+        list:[
+            {username:'ddd',message:'testddd',vip:true},
+            {username:'eee',message:'testeee',vip:false},
+            {username:'fff',message:'testfff',vip:false},
+        ]
+    }
+}
